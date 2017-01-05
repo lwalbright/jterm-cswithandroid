df6026d5d236be522866102b028d7d6201b69022
Lynn Albright <lalbright@smith.edu>
2017-01-05 11:53:21 -0800

    Turns aren't working yet

--- Scarne/app/src/main/java/com/lwalbright/scarne/MainActivity.java
+++ Scarne/app/src/main/java/com/lwalbright/scarne/MainActivity.java
@@ -3,7 +3,9 @@ package com.lwalbright.scarne;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -20,16 +22,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    protected void roll(){
+    public void rollDice(View v){
         Random rand = new Random();
-        int result = rand.nextInt(6)+1;
-        if(turn==true&&result!=1){
+        //int result = rand.nextInt(6)+1;
+        int result = 1;
+        if(turn&&result!=1){
             userTurnScore+=result;
         }
-        if(turn==false)&&result!=1){
-            compTurnScore+=1;
+        if(!turn&&result!=1){
+            compTurnScore+=result;
         }
+        if(turn&&result==1){
+            turn = false;
+            userTurnScore=0;
+        }
+        if(!turn&&result==1){
+            turn = true;
+            compTurnScore=0;
+        }
+
+        TextView tview = (TextView) findViewById(R.id.one);
+        tview.setText("Your score: "+userOverallScore+" Computer score: "+compOverallScore
+                +" \nYour turn score "+userTurnScore+" Computer turn score "+compTurnScore+turn+result);
+
         ImageView view = (ImageView) findViewById(R.id.two);
+        if(result==1){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice1)); }
+        if(result==2){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice2)); }
+        if(result==3){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice3)); }
+        if(result==4){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice4)); }
+        if(result==5){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice5)); }
+        if(result==6){
+            view.setImageDrawable(getResources().getDrawable(R.drawable.dice6)); }
+
+
 
 
     }
