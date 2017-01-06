47a73b5cd30d41be675f7eee9310a35b1c152531
Lynn Albright <lalbright@smith.edu>
2017-01-06 08:10:16 -0800

    Computer keeps rolling after it rolls a 1

--- Scarne/app/src/main/java/com/lwalbright/scarne/MainActivity.java
+++ Scarne/app/src/main/java/com/lwalbright/scarne/MainActivity.java
@@ -3,7 +3,9 @@ package com.lwalbright.scarne;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected int userTurnScore=0;
     protected int compTurnScore=0;
     protected boolean turn=true;
+    protected int result = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,45 +25,85 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void rollDice(View v){
+    private void computerTurn(){ //TODO: The issue is the computer continues to roll after it gets a 1.
+        Button holdButton = (Button) findViewById(R.id.four);
+        Button rollButton = (Button) findViewById(R.id.three);
+        holdButton.setClickable(false);
+        rollButton.setClickable(false);
+        while(compTurnScore<20&&!turn){
+            rollHelper();
+            viewUpdate(null);
+            Log.d("comp roll",result+"");
+        }
+        hold(null);
+        viewUpdate(null);
+        holdButton.setClickable(true);
+        rollButton.setClickable(true);
+    }
+
+    private void rollHelper(){
         Random rand = new Random();
-        //int result = rand.nextInt(6)+1;
-        int result = 1;
+        result = rand.nextInt(6)+1;
         if(turn&&result!=1){
             userTurnScore+=result;
         }
         if(!turn&&result!=1){
             compTurnScore+=result;
         }
-        if(turn&&result==1){
-            turn = false;
-            userTurnScore=0;
-        }
-        if(!turn&&result==1){
-            turn = true;
-            compTurnScore=0;
-        }
-
-        TextView tview = (TextView) findViewById(R.id.one);
-        tview.setText("Your score: "+userOverallScore+" Computer score: "+compOverallScore
-                +" \nYour turn score "+userTurnScore+" Computer turn score "+compTurnScore+turn+result);
-
-        ImageView view = (ImageView) findViewById(R.id.two);
         if(result==1){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice1)); }
-        if(result==2){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice2)); }
-        if(result==3){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice3)); }
-        if(result==4){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice4)); }
-        if(result==5){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice5)); }
-        if(result==6){
-            view.setImageDrawable(getResources().getDrawable(R.drawable.dice6)); }
+            if(turn){
+                userTurnScore=0;
+            }
+            if(!turn){
+                compTurnScore=0;
+            }
+            turn=!turn;
+        }
+    }
 
+    private void viewUpdate(View v){
+        TextView tview = (TextView) findViewById(R.id.one);
+        if(turn) {
+            tview.setText("Your score: " + userOverallScore + " Computer score: " + compOverallScore
+                    + " \nYour turn score " + userTurnScore + " Computer turn score " + compTurnScore + "\nYour turn!");
+        }
+        if(!turn) {
+            tview.setText("Your score: " + userOverallScore + " Computer score: " + compOverallScore
+                    + " \nYour turn score " + userTurnScore + " Computer turn score " + compTurnScore + "\nComputer's turn!");
+        }
+        ImageView view = (ImageView) findViewById(R.id.two);
+        if(result==1){view.setImageDrawable(getResources().getDrawable(R.drawable.dice1)); }
+        if(result==2){view.setImageDrawable(getResources().getDrawable(R.drawable.dice2)); }
+        if(result==3){view.setImageDrawable(getResources().getDrawable(R.drawable.dice3)); }
+        if(result==4){view.setImageDrawable(getResources().getDrawable(R.drawable.dice4)); }
+        if(result==5){view.setImageDrawable(getResources().getDrawable(R.drawable.dice5)); }
+        if(result==6){view.setImageDrawable(getResources().getDrawable(R.drawable.dice6)); }
+    }
 
+    public void rollDice(View v){
+        rollHelper();
+        viewUpdate(null);
+    }
 
+    public void reset(View v){
+        userOverallScore=0;
+        userTurnScore=0;
+        compTurnScore=0;
+        compOverallScore=0;
+        turn=true;
+        TextView tview = (TextView) findViewById(R.id.one);
+        viewUpdate(null);
+    }
 
+    public void hold(View V){
+        userOverallScore+=userTurnScore;
+        compOverallScore+=compTurnScore;
+        userTurnScore=0;
+        compTurnScore=0;
+        turn=!turn;
+        if(!turn) {
+            computerTurn();
+        }
+        viewUpdate(null);
     }
 }
